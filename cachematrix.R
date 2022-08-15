4b852cee52f4859940ce67f872d913852aa98fff
vdcgrimaldo <111251819+vdcgrimaldo@users.noreply.github.com>
2022-08-15 13:49:20 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,10 +3,23 @@
 
 ## Write a short comment describing this function
 
+library(MASS)
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<-NULL
+  set<-function(y) {
+    x<<-y
+    inv<<-NULL
+  }
+get<-function()x
+setinv<-function(inverse)inv<<-inverse
+getinv<-function() {
+  inver<-ginv(x)
+  inver%*%x
 }
-
+list (set = set, get = get, 
+      setinv=setinv, 
+      getinv = getinv)
+}
 
 ## Write a short comment describing this function
 
